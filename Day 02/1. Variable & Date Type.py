5ea4e67d825f1388042707cbcdeff3caf7e9b40e
Khan Md Hasib <32868239+hasibaust13@users.noreply.github.com>
2025-05-28 13:12:03 +1000

    Add files via upload

---
+++ Day 02/1. Variable & Date Type.py
@@ -0,0 +1,47 @@
+#.........Variables & Data Type in Python........#
+fruits = "mangoes"
+fruits = "apples"   
+print(fruits)
+
+
+# rules for naming variable
+
+#right way to declare variable
+
+#fruits = "mangoes"
+#myfav_fruits = "mangoes"
+#_fruits = "mangoes"
+#fruits1 = "mangoes"
+#fruIts = "mangoes"
+
+#wrong way to declare variable 
+
+#$fruits = "mangoes"
+#myfav fruits = "mangoes"
+#3fruits = "mangoes"
+#fru-Its = "mangoes"
+
+#Data Type in Python
+#1. number 2. string 3. booleans 4. list 5. tuple 6. set 7. dictionary
+
+#1. number data type - integer and floating point
+#integer - 10 20 25 125, the number who has no floating point
+
+x = 21
+print(x)
+
+#floating point - 10.2 20.5 125.6
+
+y = 10.5
+print(y)
+
+#booleans - True of False
+
+z = True
+w = False
+print(z)
+print(w)
+
+
+
+
