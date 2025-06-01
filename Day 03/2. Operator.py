defc915e93aa0f2fd9aa1232ef8838f062168e3a
Khan Md Hasib <32868239+hasibaust13@users.noreply.github.com>
2025-06-01 20:10:34 +1000

    Add files via upload

---
+++ Day 03/2. Operator.py
@@ -0,0 +1,93 @@
+#operator in python
+
+#Arithmetic Operator
+
+#print (10+20) #addition
+#print (10-20) #subtraction
+#print (10*20) #multiplication
+#print (12/5) #division
+#print (10**2) #exponentitation number
+#print (10%20) #reminder 
+#print (12//5) #floor division - when we want that only integer number in output then we will use floor division //
+
+#Assignment Operator
+
+#x = 4
+#y = 3
+#x += y # means x = x + y
+#print(x)
+
+#x = 4
+#y = 2
+#x -= y # means x = x - y
+#print(x)
+
+#x = 4
+#y = 3
+#x *=y # means x = x*y
+#print(x)
+
+#x = 4
+#y = 3
+#x /=y # means x = x/y
+#print(x)
+
+#x = 4
+#y = 3
+#x //=y # means x =x//y
+#print(x)
+
+#x = 4
+#y = 3
+#x **= y # means x =x**y
+#print(x)
+
+#Comparison operator true or false
+
+#x = 10
+#y = 20
+#compare = x>y #greater than operator
+#compare = x<y #less than operator
+#compare = x>=y #greater than equal operator
+#compare = x<=y #less than equal operator
+#compare = x==y #equality
+#compare = x!=y #not equality
+#print(compare)
+
+#Logical Operator 
+
+#....Truth Tabel (AND Operator)
+#TT = True 
+#TF = False
+#FT = False
+#FF = False
+
+#x = 15
+#y = 25
+#logical_and = x>y and x==y #false and false
+#print(logical_and)
+
+#x = 130
+#y = 25
+#logical_and = x>y and x>=y #true and true
+#print(logical_and)
+
+#....Truth Tabel (OR Operator)
+#TT = True 
+#TF = True
+#FT = True
+#FF = False
+
+
+#x = 130
+#y = 25
+#logical_or = x>y and x==y #true and false
+#print(logical_or)
+
+#logical not
+
+x = 15
+y = 20 
+logical_not = not x>y # logical not makes true to false and then make false to true
+print(logical_not)
+
