c416bafb33f3db2a9002e4e3587535b6942bcff4
Khan Md Hasib <32868239+hasibaust13@users.noreply.github.com>
2025-06-02 11:11:51 +1000

    Add files via upload

---
+++ Day 04/3. Number.py
@@ -0,0 +1,41 @@
+# working with numbers in python
+
+#integer #float
+
+#getting the type of numerical values....
+
+#number1 = 15
+#number2 = 15.5
+#print(type(number1)) #it shows the class of the number
+#print(type(number2)) #it shows the class of the number
+
+#number1 = "15"
+#number2 = False
+#print(type(number1)) #it shows the class of the number
+#print(type(number2)) 
+
+#Python Number Methods
+
+#..Rounding a number: round(number, ndigits)
+#pi = 3.1415926535
+#two_decimals = round(pi, 2)
+#print(two_decimals)
+
+#pi = 3.1415926535
+#four_decimals = round(pi, 4)
+#print(four_decimals)
+
+#..Raising a number to a power: pow(base,exp)
+#x = pow(4,3) # same as 4*3*3
+#print(x)
+
+#..Getting absolute value: abs(number)
+#a = abs(-24)
+#print(a)
+
+#..Getting quotient and remainder: divmod(1st number, 2nd number)
+x = divmod(10,2)
+print(x)
+
+x = divmod(14,5)
+print(x)
