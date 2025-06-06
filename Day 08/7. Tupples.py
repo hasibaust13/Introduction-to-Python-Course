4532528966450dde4a398ce617848eba3b4400ca
Khan Md Hasib <32868239+hasibaust13@users.noreply.github.com>
2025-06-06 23:51:48 +1000

    Add files via upload

---
+++ Day 08/7. Tupples.py
@@ -0,0 +1,8 @@
+#............Tuples in Python.........
+pets = ("dog", "cat", "rabbit") #Tuple
+#pets = ["dog", "cat", "rabbit"] #list
+#print(pets)
+#print(type(pets))
+#print(pets[1])  #We can use the index number of tuple to get the specific result
+#pets[1] = "fish" #it gives error as in tuple, we cannot change the variable otherwise the program crash
+print(pets)
