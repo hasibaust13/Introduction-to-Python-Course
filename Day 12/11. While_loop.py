13aa33e8d2cafaebe047e68181f8d106265c6416
Khan Md Hasib <32868239+hasibaust13@users.noreply.github.com>
2025-06-10 08:38:51 +1000

    Add files via upload

---
+++ Day 12/11. While_loop.py
@@ -0,0 +1,12 @@
+#.................While Loop in Python...........
+''''
+while condition:
+    statements
+
+'''
+#.....creating hello world for fifty times
+i = 1  #...loop iniatialization
+while i<=5: #.....loop condition
+    #while loop body
+    print("Hello World")
+    i += 1 #increament or decreament of the loop
