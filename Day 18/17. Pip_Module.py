48d661f56ea3d8e00d6d6f91e84910911f608215
Khan Md Hasib <32868239+hasibaust13@users.noreply.github.com>
2025-06-20 22:10:39 +1000

    Add files via upload

---
+++ Day 18/17. Pip_Module.py
@@ -0,0 +1,32 @@
+# ................Pip & Module in Python.............
+# math module in python
+
+# import math
+# number = 2
+# result = math.sqrt(number) #square root of a number
+# print(result)
+
+# number1 = 2
+# number2 = 3
+# result = math.pow(number1, number2) #power of a number
+# print(result)
+
+# number = 2.5
+# result = math.ceil(number) #convert the number from float to integer
+# print(result)
+
+# pi = math.pi
+# print(pi)
+
+# number = 2
+# result = math.cos(number) #get the number of cos 2
+# print(result)
+
+# date and time modules
+# import datetime
+# x = datetime.datetime.now() #current date
+# print(x)
+
+from datetime import date
+x = date.today()  # only to see the current date
+print(x)
